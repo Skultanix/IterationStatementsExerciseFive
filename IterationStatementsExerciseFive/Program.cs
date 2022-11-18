169f2fc52d71c840abb6afff44490d416b5ac8a3
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-11-17 17:02:14 -0700

    Part five complete.

--- IterationStatementsExerciseFive/Program.cs
+++ IterationStatementsExerciseFive/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(@"Please enter in a series of numbers each separated by a comma and a space ("", ""). ");
+            var userSeries = Console.ReadLine();
+            int[] seriesArray = userSeries.Split(',').Select(n => Convert.ToInt32(n)).ToArray();
+            var maxVal = 0;
+
+            foreach (var number in seriesArray)
+            {
+                if (number > maxVal)
+                {
+                    maxVal = number;
+                }
+            }
+
+            Console.WriteLine(string.Format(@"The maximum value in the series is ""{0}"".", maxVal));
         }
     }
 }
